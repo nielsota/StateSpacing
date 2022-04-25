5354eec3fbb0525791dc0cd88f91216149d13874
Niels Ota <niels@ianl.onmicrosoft.com>
2022-04-25 17:01:39 +0200

    25-05-2022: protocols finished, started first implementation of kalman protocol

---
+++ statespace/StateSpacingProtocols.py
@@ -0,0 +1,41 @@
+from typing import List, Protocol
+
+import numpy as np
+
+class MathProtocol(Protocol):
+    
+    def _mm(M1: np.ndarray, M2: np.ndarray, dtype=np.float64) -> np.ndarray:
+        ...
+
+    def _bmm(M1: np.ndarray, M2: np.ndarray, dtype=np.float64)-> np.ndarray:
+        ...
+    
+    def _T(M1: np.ndarray, dtype=np.float64) -> np.ndarray:
+        ...
+
+    def _inv(M1: np.ndarray, dtype=np.float64) -> np.ndarray:
+        ...
+
+    def _mm3(M1: np.ndarray, M2: np.ndarray, M3: np.ndarray, dtype=np.float64)-> np.ndarray:
+        ...
+
+    def _bmm3(M1: np.ndarray, M2: np.ndarray, M3: np.ndarray, dtype=np.float64)-> np.ndarray:
+        ...
+
+class KalmanProtocol(Protocol):
+
+    def kalman_step(*args) -> List[np.ndarray]:
+        ...
+
+    def kalman_step_missing(*args) -> List[np.ndarray]:
+        ...
+    
+    def kalman_filter(*args) -> List[np.ndarray]:
+        ...
+    
+    def kalman_smoother(*args) -> List[np.ndarray]:
+        ...
+
+    def kalman_forecast(*args, time=10, dtype=np.float64) -> List[np.ndarray]:
+        ...
+
